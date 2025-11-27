62f5e5a70e34e971b64957c6b9ad23980fca9be9
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2025-11-27 16:09:39 -0300

    Update CanvasKit integration and service worker
    Updated CanvasKit-related logic in main.dart.js, including function and variable renames, and improved browser detection. Updated flutter_bootstrap.js to use a new service worker version. Refreshed resource hashes and references in flutter_service_worker.js to match the latest build artifacts.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3443084680"
+    serviceWorkerVersion: "4261202289"
   }
 });
