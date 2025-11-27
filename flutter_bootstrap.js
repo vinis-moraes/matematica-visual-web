5d631c01e45ca0f3680e3025fea3486f370b4489
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2025-11-27 15:43:08 -0300

    Update web build artifacts and service worker
    Regenerated flutter_bootstrap.js, flutter_service_worker.js, index.html, and main.dart.js. Updates include service worker version, resource hashes, and minor HTML cleanup for Firebase SDK and service worker registration.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "106962647"
+    serviceWorkerVersion: "3112852824"
   }
 });
