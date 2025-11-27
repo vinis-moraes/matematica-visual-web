41e15be9769ec13d9c6a5c8cb26014ded861b299
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2025-11-27 15:57:06 -0300

    Update service worker and main Dart JS logic
    Bumped service worker version and updated resource hashes in flutter_service_worker.js. Modified main.dart.js to improve FCM service worker registration logging with an init check message.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2154201040"
+    serviceWorkerVersion: "3701666783"
   }
 });
