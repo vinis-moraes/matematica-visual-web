9a7a1c78f76b5381415e25130187aeb20744646d
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2025-12-02 08:52:30 -0300

    Update build artifacts and service worker version
    Regenerated build files including main.dart.js, flutter_service_worker.js, flutter_bootstrap.js, and MaterialIcons-Regular.otf. Updated service worker version and resource hashes to reflect new build output.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2245876906"
+    serviceWorkerVersion: "2261270613"
   }
 });
